a40752071a75f2b16f33fd682359b6a3a3f1aba4
NewtalentsG <newtalents403@gmail.com>
2025-04-15 21:48:05 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -47,19 +47,16 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
+  // setup vite in development
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = 5000;
+  // use dynamic port for Railway compatibility
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 5000;
+
   server.listen({
     port,
     host: "0.0.0.0",
